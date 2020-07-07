c0f8f8e35c72448599001cae363eff88a649c050
Vuqar Yusifli <vuqar.yusifli.94@gmail.com>
2020-07-08 00:18:16 +0400

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Post from "./components/Post";
-import Page from "./components/Page";
+import React, {lazy, Suspense} from 'react';
 import {Route, Switch} from "react-router-dom";
+import Navbar from "./components/Navbar";
+const Home = lazy(() => import("./components/Home"));
+const Post = lazy(() => import("./components/Post"));
+const Page = lazy(() => import("./components/Page"));
 
 const App: React.FC = () => {
     return (
         <div className="App">
             <Navbar/>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/create" component={Page}/>
-                <Route exact path="/edit/:id" component={Page}/>
-                <Route exact path="/post/:id" component={Post}/>
-            </Switch>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/create" component={Page}/>
+                    <Route exact path="/edit/:id" component={Page}/>
+                    <Route exact path="/post/:id" component={Post}/>
+                </Switch>
+            </Suspense>
         </div>
     );
 };
